a74f7829865307720f6a7fc8e52221e74576eee1
luobin <rockamring@126.com>
2018-05-24 22:01:11 +0800

    startup

---
+++ server/startup.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"github.com/rockamring/goproject/server/module"
+	"github.com/rockamring/goproject/server/conf"
+	"github.com/rockamring/goproject/server/log"
+	"os"
+	"os/signal"
+)
+
+func Run(mods ...module.Module) {
+	// logger
+	if conf.LogLevel != "" {
+		logger, err := log.New(conf.LogLevel, conf.LogPath, conf.LogFlag)
+		if err != nil {
+			panic(err)
+		}
+		log.Export(logger)
+		defer logger.Close()
+	}
+
+	log.Release("server %v starting up", version)
+
+	// module
+	for i := 0; i < len(mods); i++ {
+		module.Register(mods[i])
+	}
+	module.Init()
+
+
+	// close
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	log.Release("server closing down (signal: %v)", sig)
+
+	module.Destroy()
+}
---
+++ server/version.go
@@ -0,0 +1,3 @@
+package server
+
+const version  = "1.0"
